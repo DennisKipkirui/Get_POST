ad46859eda4f55b2223eff21a85c6be18a66693b
DennisKipkirui <kipkiruimutai33@gmail.com>
2022-07-31 07:54:25 +0300

    pass variable one page to another page or same page

---
+++ get_post/index.js
@@ -0,0 +1,9 @@
+function handleSubmit ()
+const name = document.getElementById('name').value;
+const surname = document.getElementById('surname').value;
+
+
+localStorage.setItem("NAME", NAME);
+localStorage.setItem("SURNAME", NAME);
+
+return;
